5d178221e0e594af3ae4ecdac2d1ee2150d0e751
RogueEntity <66661017+rogueEntity@users.noreply.github.com>
2020-12-29 22:53:01 +0900

    #2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -2,34 +2,40 @@ function Food({name, picture}) {
   return (
     <div>
       <h2>I like {name}</h2>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id:1,
     name: "Pasta",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fassets.epicurious.com%2Fphotos%2F55f72d733c346243461d496e%2F2%3A1%2Fw_1260%252Ch_630%2F09112015_15minute_pastasauce_tomato.jpg&f=1&nofb=1"
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.visitbend.com%2Fwp-content%2Fuploads%2F2018%2F05%2Fpastini-pasta.jpg&f=1&nofb=1"
   },
   {
+    id:2,
     name: "Cutlet",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpull-revisfoodography.netdna-ssl.com%2Fwp-content%2Fuploads%2F2017%2F01%2Fveg-cutlet-1.jpg&f=1&nofb=1"
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F1%2F16%2FTonkatsu_by_luckypines.jpg%2F180px-Tonkatsu_by_luckypines.jpg&f=1&nofb=1"
   },
   {
+    id:3,
     name: "Beef Bourguignon",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fshare1.canvasholidays.com%2Fblog%2Fapp%2Fuploads%2F2015%2F06%2Fbeef-bourguignon.jpg&f=1&nofb=1"
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.tasteofhome.com%2Fwp-content%2Fuploads%2F2018%2F01%2FPressure-Cooker-Boeuf-Bourguignon_exps207902_HC143213C03_18_3bC_RMS-1.jpg%3Fresize%3D150%2C150&f=1&nofb=1"
   },
   {
-    name: "Cream Cheese Cake",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi1.wp.com%2Fwww.livewellbakeoften.com%2Fwp-content%2Fuploads%2F2017%2F04%2FCarrot-Cake-with-Cream-Cheese-Frosting-4.jpg%3Fresize%3D1360%252C2040%26ssl%3D1&f=1&nofb=1"
+    id:4,
+    name: "Cheese Cake",
+    image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.epicurious.com%2Fimages%2Farticlesguides%2Fhowtocook%2Fdishes%2Fcakes%2Fcheese-cake_116.jpg&f=1&nofb=1"
   }
 ];
 
 function App() {
   return (
     <div className="App">
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
