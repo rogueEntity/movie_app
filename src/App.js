dcaf8db45459992bdee91fd1b96191016fef5d2c
RogueEntity <66661017+rogueEntity@users.noreply.github.com>
2021-02-07 14:24:57 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -2,39 +2,20 @@ import React from "react";
 import {Component} from "react";// eslint-disable-line no-unused-vars
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("Component constructed");
-  }
   state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  };
-
-  remove = () => {
-    this.setState(current => ({count: current.count - 1}));
-  };
+    isLoading: true,
+    movies: []
+  }
 
   componentDidMount() {
-    console.log("Component rendered");
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
   }
 
-  componentDidUpdate() {
-    console.log("Component updated");
-  }
-  
   render() {
-    console.log("Component rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.remove}>remove</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
